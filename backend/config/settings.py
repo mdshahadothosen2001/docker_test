d0ebd9ea532dc0aa1171cd514febbd428a9e3650
mdshahadothosen2001 <mdshahadothosen2001@gmail.com>
2023-11-20 09:29:53 +0600

    feat: setup postgresql.

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
-
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -59,10 +60,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DB_CONNECTION"),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
